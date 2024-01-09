754d051e412cc8f41496626729959013a4051a1e
Shahboz <sharafzodashahboz@gmail.com>
2024-01-09 15:43:39 +0500

    pydantic schema added, nested models

---
+++ schemas.py
@@ -0,0 +1,24 @@
+from enum import Enum 
+from datetime import date
+from pydantic import BaseModel
+
+
+class GenreURLChoices(Enum):
+    ROCK = 'rock'
+    ELECTRONIC = 'electronic'
+    METAL = 'metal'
+    HIP_HOP = 'hip-hop'
+
+
+class Album(BaseModel):
+    title: str
+    release_date: date
+
+class Band(BaseModel):
+
+    id: int
+    name: str 
+    genre: str 
+    albums: list[Album] = []
+
+
