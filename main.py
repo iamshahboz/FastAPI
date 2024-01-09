3ecf377bfd8c0465ab5cddb93d7fb7aad5734767
Shahboz <sharafzodashahboz@gmail.com>
2024-01-09 15:44:03 +0500

    enum class separated to another file

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
 from fastapi import FastAPI, HTTPException
-from enum import Enum 
+from schemas import GenreURLChoices, Band
+
 
 app = FastAPI()
 
 # don't forget to add type hints
 
 
-class GenreURLChoices(Enum):
-    ROCK = 'rock'
-    ELECTRONIC = 'electronic'
-    METAL = 'metal'
-    HIP_HOP = 'hip-hop'
+
 
 BANDS = [
     {'id':1, 'name': 'The kinks', 'genre': 'Rock'},
     {'id':2, 'name': 'Aphex Twin', 'genre': 'Electronic'},
-    {'id':3, 'name': 'Slowdive', 'genre': 'Shoegaze'},
+    {'id':3, 'name': 'Slowdive', 'genre': 'Shoegaze', 'albums': [
+        {'title': 'Master of Reality', 'release_date': '1971-07-21'}
+    ]},
     {'id':4, 'name': 'Wu Tang Chan', 'genre': 'Hip-Hop'},
 ]
 
 
 @app.get('/bands')
-async def bands() -> list[dict]:
-    return BANDS
+async def bands() -> list[Band]:
+    return [
+        Band(**b) for b in BANDS
+    ]
 
 @app.get('/bands/{band_id}')
-async def bands(band_id: int) -> dict:
-    band = next((b for b in BANDS if b['id'] == band_id),None)
+async def bands(band_id: int) -> Band:
+    band = next((Band(**b) for b in BANDS if b['id'] == band_id),None)
     if band is None:
         raise HTTPException(status_code=404, detail='Band not found')
     return band 
