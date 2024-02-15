5d8d615846cbecd53e9b67a7fe96407186fc7080
Shahboz <sharafzodashahboz@gmail.com>
2024-02-16 01:25:56 +0500

    query parameters and filtering

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from schemas import GenreURLChoices, Band
+from schemas import GenreURLChoices, Band, Album
 
 
 app = FastAPI()
@@ -20,10 +20,18 @@ BANDS = [
 
 
 @app.get('/bands')
-async def bands() -> list[Band]:
-    return [
-        Band(**b) for b in BANDS
-    ]
+async def bands(
+    genre: GenreURLChoices | None=None,
+    has_albums: bool = False
+    ) -> list[Band]:
+    band_list = [Band(**b) for b in BANDS]
+    if genre:
+        band_list =  [
+            b for b in band_list if b.genre.lower() == genre.value
+        ]
+    if has_albums:
+        band_list = [b for b in band_list if len(b.albums) > 0]
+    return band_list
 
 @app.get('/bands/{band_id}')
 async def bands(band_id: int) -> Band:
